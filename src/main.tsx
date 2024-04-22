e1758c2b43ffb159b8f74c0b84b688c7e64ca0cb
Scott Benton <scott@scottbenton.dev>
2024-04-22 02:27:57 -0400

    feat(PWA): Added PWA capabilities

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,30 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "@fontsource-variable/plus-jakarta-sans";
 import { AppProviders } from "./providers/AppProviders.tsx";
+import { registerSW } from "virtual:pwa-register";
+
+const intervalMS = 60 * 60 * 1000;
+registerSW({
+  immediate: true,
+  onRegisteredSW(swUrl, r) {
+    r &&
+      setInterval(async () => {
+        if (!(!r.installing && navigator)) return;
+
+        if ("connection" in navigator && !navigator.onLine) return;
+
+        const resp = await fetch(swUrl, {
+          cache: "no-store",
+          headers: {
+            cache: "no-store",
+            "cache-control": "no-cache",
+          },
+        });
+
+        if (resp?.status === 200) await r.update();
+      }, intervalMS);
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,47 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        skipWaiting: true,
+      },
+      includeAssets: [
+        "icon512_rounded.png",
+        "icon512_maskable.png",
+        "MiseLogo192.png",
+      ],
+      manifest: {
+        name: "Mise",
+        short_name: "Mise",
+        description: "Mise is an app for organizing your work",
+        theme_color: "#0f172a",
+        icons: [
+          {
+            src: "MiseLogo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon512_rounded.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon512_maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
