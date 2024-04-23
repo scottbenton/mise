d053374414c10c741593ad18dac12757f5478923
Scott Benton <scott@scottbenton.dev>
2024-04-23 00:51:49 -0400

    fix(mobile): Fixed mobile view

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -1,16 +1,12 @@
-import { Container, useMediaQuery, useTheme } from "@mui/material";
+import { Container } from "@mui/material";
 import { PropsWithChildren } from "react";
 
 export function PageLayout(props: PropsWithChildren) {
   const { children } = props;
 
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-
   return (
     <Container
       maxWidth={"lg"}
-      disableGutters={isMobile}
       sx={{
         minHeight: "100%",
         display: "flex",
